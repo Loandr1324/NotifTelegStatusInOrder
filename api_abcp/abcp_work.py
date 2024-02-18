fed759d160c66bba7e6f6fc188280bddef48b9f6
Loik Andrey <loikand80@gmail.com>
2024-02-19 00:31:39 +1000

    Для тестов добавлена запись словаря подобное возвращаемой ошибки с ABCP

--- api_abcp/abcp_work.py
+++ api_abcp/abcp_work.py
@@ -48,7 +48,10 @@ class WorkABCP:
         """
 
         try:
-            # raise Exception('Какой-то текст') # Используем вызов ошибки при тестах
+            # raise Exception({
+            #     "errorCode": 200,
+            #     "errorMessage": "Позиция 151056635 уже была отправлена поставщику в заказ ранее."
+            # })  # Используем вызов ошибки при тестах
             result = await self.api_abcp.cp.admin.orders.send_online_order(
                 order_params=order_params,
                 positions=positions
