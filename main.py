d7233df566db22999ede572e84ec42ac9821dfd6
Loik Andrey <loikand80@gmail.com>
2024-06-25 21:04:58 +1000

    Удалён старый код

--- main.py
+++ main.py
@@ -24,38 +24,13 @@ def main() -> None:
     wk_g = ''
     try:
         logger.info(f"Получаем список задач")
-        # wk_g = WorkGoogle()
-        # tasks = wk_g.get_tasks()
         wk_tasks = Tasks()
         tasks = wk_tasks.check_tasks_scheduler()
     except Exception as e:
         logger.error(f"Ошибка при получении списка задач: {e}")
         return
-    logger.warning(f"Получили задачи {tasks}")  # TODO Удалить после тестов
+    # logger.warning(f"Получили задачи {tasks}")  # TODO Удалить после тестов
     for task in tasks:
-        # if task['task_id'] == '1_1' and task['can_run_task']:   # TODO Удалить после тестов
-        #     try:
-        #         logger.info(f"Запускаем выполнение задачи по {task['task_name']}")
-        #         notif = Notif(
-        #             task_id=task['task_id'],
-        #             status_notif=task['status_id'],
-        #             repeat_notification=task['repeat'],
-        #             date_start=task['date_start'],
-        #             text_message=task['temp_not1'],
-        #             text_message_repeat=task['temp_not2'],
-        #             type_notif=task['type_notif'],
-        #         )
-        #         notif.start_notif()
-        #
-        #         # Добавляем запись о последнем запуске задачи в Google Sheets
-        #         date_now = dt.now()
-        #         WorkGoogle().set_tasks_last_start(
-        #             row=task['row_task_on_sheet'],
-        #             value=date_now.strftime('%Y-%m-%d %H:%M:%S')
-        #         )
-        #     except Exception as e:
-        #         logger.error(f"Произошла ошибка при выполнении задачи по отправке уведомлений {e}")
-
         if task['select_alg'].lower() == 'оформление заказа поставщику' and task['can_run_task']:
             logger.info(f"Запускаем выполнение задачи: {task['task_id']} по {task['task_name']}")
             sup_reorder = ReOrder(
@@ -74,24 +49,7 @@ def main() -> None:
                 row=task['row_task_on_sheet'],
                 value=date_now.strftime('%Y-%m-%d %H:%M:%S')
             )
-        #
-        # elif task['task_id'] == '6' and task['can_run_task']:  # TODO Удалить после тестов
-        #     logger.warning(f"Запускаем выполнение задачи по {task['task_name']}")
-        #     notif = Notif(
-        #         task_id=task['task_id'],
-        #         status_notif=task['status_id'],
-        #         repeat_notification=task['repeat'],
-        #         date_start=task['date_start'],
-        #         allowed_suppliers=task['allowed_suppliers']
-        #     )
-        #     notif.start_notif()
-        #
-        #     # Добавляем запись о последнем запуске задачи в Google Sheets
-        #     date_now = dt.now()
-        #     WorkGoogle().set_tasks_last_start(
-        #         row=task['row_task_on_sheet'],
-        #         value=date_now.strftime('%Y-%m-%d %H:%M:%S')
-        #     )
+
         elif task['select_alg'].lower() == 'уведомления' and task['can_run_task']:
             logger.info(f"Запускаем выполнение задачи: {task['task_id']} по {task['task_name']}")
             notif = Notif(
